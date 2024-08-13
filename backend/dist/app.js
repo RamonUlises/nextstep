96395c3af5ebb8e0b4fadc84232511fddf463d42
RamonUlises <jorgitomilina@gmail.com>
2024-08-12 23:53:05 -0600

    chore: Remove unnecessary files and update outDir in tsconfig.json

--- backend/dist/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import './database/connection';
-const app = express();
-const port = process.env.PORT ?? 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port.toString()}`);
-});
