c30b6d9c35a2c6ea1347ec65ce29c1ef10342112
RamonUlises <jorgitomilina@gmail.com>
2024-08-14 10:41:04 -0600

    feat: CRUD in empresas

--- backend/src/controllers/empresas.ts
+++ backend/src/controllers/empresas.ts
@@ -2,7 +2,7 @@ import zod from 'zod';
 import crypto from 'node:crypto';
 import { Request, Response } from 'express';
 
-import requestDatabase from '@/database/request/empresas';
+import requestDataBaseEmpresa from '@/database/request/empresas';
 import { TypeEmpresa } from '@/types/empresas';
 
 const { string } = zod;
@@ -10,14 +10,28 @@ const { string } = zod;
 class Empresa {
   async obtenerEmpresas(req: Request, res: Response): Promise<void> {
     try {
-      const data = await requestDatabase.obtenerEmpresas();
+      const data: TypeEmpresa[] = await requestDataBaseEmpresa.obtenerEmpresas();
 
       res.status(200).json({ message: 'Empresas obtenidas', data });
     } catch (error) {
       res.status(500).json({ message: 'Error al obtener las empresas', error });
     }
   }
+  async obtenerEmpresa(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const data: TypeEmpresa[] = await requestDataBaseEmpresa.obtenerEmpresa(id);
 
+      if(data.length === 0) {
+        res.status(404).json({ message: 'Empresa no encontrada' });
+        return;
+      }
+
+      res.status(200).json({ message: 'Empresa obtenida', data });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al obtener la empresa', error });
+    }
+  };
   async crearEmpresa(req: Request, res: Response): Promise<void> {
     try {
 
@@ -42,11 +56,66 @@ class Empresa {
       schema.parse(req.body);
 
       data.id = crypto.randomUUID();
-      await requestDatabase.crearEmpresa(data);
+      await requestDataBaseEmpresa.crearEmpresa(data);
 
       res.status(200).json({ message: 'Empresa creada' });
     } catch (error) {
       res.status(400).json({ message: 'Le faltan datos a la petición', error });
+    }
+  }
+  async actualizarEmpresa(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const emp: TypeEmpresa[] = await requestDataBaseEmpresa.obtenerEmpresa(id);
+
+      if(emp.length === 0) {
+        res.status(404).json({ message: 'Empresa no encontrada' });
+        return;
+      }
+
+      const data: TypeEmpresa = req.body as TypeEmpresa;
+
+      const schema = zod.object({
+        'numero-identificacion': string(),
+        'certificado-registro': string(),
+        'licencia-comercial': string(),
+        'nombre': string(),
+        'direccion': string().array(),
+        'telefono': string().array(),
+        'email': string().array(),
+        'contrasena': string(),
+        'redes-sociales': string().array(),
+        'mision': string(),
+        'vision': string(),
+        'objetivos': string().array(),
+        'imagen': string(),
+      });
+
+      schema.parse(req.body);
+
+      await requestDataBaseEmpresa.actualizarEmpresa(id, data);
+
+      res.status(200).json({ message: 'Empresa actualizada' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al actualizar la empresa', error });
+    }
+  }
+  async eliminarEmpresa(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+
+      const data: TypeEmpresa[] = await requestDataBaseEmpresa.obtenerEmpresa(id);
+
+      if(data.length === 0) {
+        res.status(404).json({ message: 'Empresa no encontrada' });
+        return;
+      }
+
+      await requestDataBaseEmpresa.eliminarEmpresa(id);
+
+      res.status(200).json({ message: 'Empresa eliminada' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al eliminar la empresa', error });
     }
   }
 }
--- backend/src/database/request/empresas.ts
+++ backend/src/database/request/empresas.ts
@@ -2,20 +2,43 @@ import { TypeEmpresa } from '@/types/empresas';
 import empresas from '@schemas/empresas';
 
 class RequesDatabase {
-  async obtenerEmpresas(): Promise<typeof empresas[]> {
+  async obtenerEmpresas(): Promise<TypeEmpresa[]> {
     try{
-      const data: typeof empresas[] = await empresas.find();
-      return data;
+      const data = await empresas.find();
+      return data as TypeEmpresa[];
     } catch{ 
       throw new Error('Error al obtener las empresas');
     }
   }
-
+  async obtenerEmpresa(id: string): Promise<TypeEmpresa[]> {
+    try {
+      const data = await empresas.find({ id });
+      return data as TypeEmpresa[];
+    } catch(error) {
+      console.log(error);
+      throw new Error('Error al obtener la empresa');
+    }
+  }
   async crearEmpresa(data: TypeEmpresa): Promise<void> {
     try {
       await empresas.create(data);
     } catch {
       throw new Error('Error al crear la empresa');
+    }
+  }
+  async actualizarEmpresa(id: string, data: Partial<TypeEmpresa>): Promise<void> {
+    try {
+      delete data.id;
+      await empresas.updateOne({ id }, data);
+    } catch {
+      throw new Error('Error al actualizar la empresa');
+    }
+  }
+  async eliminarEmpresa(id: string): Promise<void> {
+    try {
+      await empresas.deleteOne({ id });
+    } catch {
+      throw new Error('Error al eliminar la empresa');
     }
   }
 }
