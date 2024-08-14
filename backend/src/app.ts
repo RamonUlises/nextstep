b9f00eca4a86996d38dee3d6f2d538eaca16ac5a
RamonUlises <jorgitomilina@gmail.com>
2024-08-13 20:05:31 -0600

    add config morgan and cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import morgan, { StreamOptions } from 'morgan';
 import '@database/verifyIp';
 
 const app: express.Application = express();
 const port: number | string = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(morgan('dev') as express.RequestHandler<StreamOptions>);
 
 export default function createServer(): void{
   app.listen(port, () => {
