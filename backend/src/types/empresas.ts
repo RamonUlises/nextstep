657232782b81bae37f10cd0c34dcd70caf0dee55
RamonUlises <jorgitomilina@gmail.com>
2024-08-14 09:51:50 -0600

    feat: Add TypeEmpresa interface to backend

---
+++ backend/src/types/empresas.ts
@@ -0,0 +1,16 @@
+export interface TypeEmpresa {
+  'id': string;
+  'numero-identificacion': string;
+  'certificado-registro': string;
+  'licencia-comercial': string;
+  'nombre': string;
+  'direccion': string[];
+  'telefono': string[];
+  'email': string[];
+  'contrasena': string;
+  'redes-sociales': string[];
+  'mision': string;
+  'vision': string;
+  'objetivos': string[];
+  'imagen': string;
+};
