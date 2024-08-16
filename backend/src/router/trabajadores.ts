d136b4b82c91995f6497365c54e892ff139e7964
RamonUlises <jorgitomilina@gmail.com>
2024-08-16 16:14:48 -0600

    chore: Update trabajadores.ts router to use controllers

--- backend/src/router/trabajadores.ts
+++ backend/src/router/trabajadores.ts
@@ -1,5 +1,26 @@
+import trabajadores from '@/controllers/trabajadores';
 import { Router } from 'express';
 
 const router: Router = Router();
+
+router.get('/', (req, res) => {
+  void trabajadores.obtenerTrabajadores(req, res);
+});
+
+router.get('/:id', (req, res) => {
+  void trabajadores.obtenerTrabajador(req, res);
+});
+
+router.post('/', (req, res) => {
+  void trabajadores.crearTrabajador(req, res);
+});
+
+router.put('/:id', (req, res) => {
+  void trabajadores.actualizarTrabajador(req, res);
+});
+
+router.delete('/:id', (req, res) => {
+  void trabajadores.eliminarTrabajador(req, res);
+});
 
 export default router;
