e8af47d027e281d23108f6e96598b3292c31638c
RamonUlises <jorgitomilina@gmail.com>
2024-08-16 19:28:10 -0600

    feat: Add 'Trabajos' interface definition

---
+++ backend/src/types/trabajos.ts
@@ -0,0 +1,14 @@
+export interface TypeTrabajos {
+  'id': string;
+  'empresa': string;
+  'descripcion': string;
+  'requerimientos': string[];
+  'fecha-publicacion': string;
+  'fecha-inicio': string;
+  'fecha-expericacion': string;
+  'contrato': string;
+  'presupuesto': number;
+  'puntos': number;
+  'etiquetas': string[];
+  'departamento': string[];
+};
