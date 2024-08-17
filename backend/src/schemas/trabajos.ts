0a920a8d72132a43819f9c5d305906105b922fe4
RamonUlises <jorgitomilina@gmail.com>
2024-08-16 19:28:41 -0600

    refactor: Update 'trabajos.ts' schema to use string type for date fields

--- backend/src/schemas/trabajos.ts
+++ backend/src/schemas/trabajos.ts
@@ -1,18 +1,18 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
-const trabajosSchema: Schema = new mongoose.Schema({
+const trabajosSchema = new mongoose.Schema({
   'id': { type: String, required: true },
   'empresa': { type: String, required: true },
   'descripcion': { type: String, required: true },
   'requerimientos': { type: Array, required: true },
-  'fecha-publicacion': { type: Date, required: true },
-  'fecha-inicio': { type: Date, required: true },
-  'fecha-expericacion': { type: Date, required: true },
+  'fecha-publicacion': { type: String, required: true },
+  'fecha-inicio': { type: String, required: true },
+  'fecha-expericacion': { type: String, required: true },
   'contrato': { type: String, required: true },
   'presupuesto': { type: Number, required: true },
   'puntos': { type: Number, required: true },
   'etiquetas': { type: Array, required: true },
-  'departamento': { type: String, required: true },
+  'departamento': { type: Array, required: true },
 });
 
 const Trabajos = mongoose.model('Trabajos', trabajosSchema);
