4cf678832968c270cc90f5dab7973a136bb32f83
RamonUlises <jorgitomilina@gmail.com>
2024-08-16 19:28:24 -0600

    feat: Add requestDatabase methods for CRUD operations on 'Trabajos'

---
+++ backend/src/database/request/trabajos.ts
@@ -0,0 +1,47 @@
+import trabajos from '@/schemas/trabajos';
+import { TypeTrabajos } from '@/types/trabajos';
+
+class RequesDatabase {
+  async obtenerTrabajos(): Promise<TypeTrabajos[]> {
+    try {
+      const data = await trabajos.find();
+      return data as TypeTrabajos[];
+    } catch {
+      throw new Error('Error al obtener los trabajos');
+    }
+  }
+  async obtenerTrabajo(id: string): Promise<TypeTrabajos[]> {
+    try {
+      const data = await trabajos.find({ id });
+      return data as TypeTrabajos[];
+    } catch {
+      throw new Error('Error al obtener el trabajo');
+    }
+  }
+  async crearTrabajo(data: TypeTrabajos): Promise<void> {
+    try {
+      await trabajos.create(data);
+    } catch {
+      throw new Error('Error al crear el trabajo');
+    }
+  }
+  async actualizarTrabajo(id: string, data: Partial<TypeTrabajos>): Promise<void> {
+    try {
+      delete data.id;
+      await trabajos.updateOne({ id }, data);
+    } catch {
+      throw new Error('Error al actualizar el trabajo');
+    }
+  }
+  async eliminarTrabajo(id: string): Promise<void> {
+    try {
+      await trabajos.deleteOne({ id });
+    } catch {
+      throw new Error('Error al eliminar el trabajo');
+    }
+  }
+}
+
+const requestDatabase = new RequesDatabase();
+
+export default requestDatabase;
