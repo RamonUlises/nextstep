cf6a04cfbd949b1b256140bce9d8a4aed0d0bdfd
RamonUlises <jorgitomilina@gmail.com>
2024-08-19 06:59:51 -0600

    refactor: Update app.ts to use verifyAuth middleware for authentication

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import cors from 'cors';
 import morgan, { StreamOptions } from 'morgan';
 import trabajos from '@router/trabajos';
 import trabajadores from '@router/trabajadores';
 import empresas from '@router/empresas';
 import '@database/connection';
-//import { verifyAuth } from '@lib/verifyAuth';
+import { verifyAuth } from '@lib/verifyAuth';
 
 const app: express.Application = express();
 const port: number | string = process.env.PORT ?? 3000;
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev') as express.RequestHandler<StreamOptions>);
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   const response: boolean = verifyAuth(req, res);
-//   if(response) next();
-// });
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const response: boolean = verifyAuth(req, res);
+  if(response) next();
+});
 
 app.use('/api/trabajos', trabajos);
 app.use('/api/trabajadores', trabajadores);
@@ -28,7 +28,7 @@ app.use('/api/empresas', empresas);
 app.use((req, res) => {
   res.status(404).json({
     message: 'La ruta no existe',
-    documentation: 'https://github.com/RamonUlises/startup-pixels/blob/main/backend/README.md',
+    documentation: 'https://github.com/RamonUlises/nextstep/blob/main/backend/README.md',
   });
 });
 
