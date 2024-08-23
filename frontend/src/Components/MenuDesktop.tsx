e01fd122f1e6b1c595a7e4621aaf43330004a346
RamonUlises <jorgitomilina@gmail.com>
2024-08-23 11:34:09 -0600

    refactor: Actualizar tema oscuro en menú y botón de inicio

--- frontend/src/Components/MenuDesktop.tsx
+++ frontend/src/Components/MenuDesktop.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { ChangeTheme } from "./ChangeTheme";
 
 export const MenuDesktop = () => {
@@ -5,54 +7,54 @@ export const MenuDesktop = () => {
     <>
       <header className="flex justify-between text-white p-4 fixed w-full bg-transparent">
         <section className="flex items-center bg-gradient-to-tl to-[#C3480B] from-[#E75F0B] py-4 px-10 rounded-lg gap-10 dark:to-zinc-700 dark:from-zinc-800">
-          <a href="/" className="font-bold text-2xl">
+          <Link to="/" className="font-bold text-2xl">
             NextStep
-          </a>
+          </Link>
           <nav>
             <ul
               className="flex items-center gap-6 text-base ml-4
             font-medium"
             >
               <li>
-                <a
-                  href="/"
-                  className="hover:opacity-80 transition-opacity duration-300"
+                <Link
+                  to="/"
+                  className="hover:opacity-80 transition-opacity duration-300 underline decoration-2"
                 >
                   Inicio
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/trabajo"
+                <Link
+                  to="/trabajo"
                   className="hover:opacity-80 transition-opacity duration-300"
                 >
                   Trabajo
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/trabajadores"
+                <Link
+                  to="/trabajadores"
                   className="hover:opacity-80 transition-opacity duration-300"
                 >
                   Trabajadores
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/empresas"
+                <Link
+                  to="/empresas"
                   className="hover:opacity-80 transition-opacity duration-300"
                 >
                   Empresas
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
         </section>
         <section className="flex items-center bg-gradient-to-tl to-[#C3480B] from-[#E75F0B] py-4 px-6 gap-5 rounded-lg justify-center dark:to-zinc-700 dark:from-zinc-800">
           <ChangeTheme />
-          <button className="button-menu before:bg-gradient-to-tl before:to-[#C3480B] before:from-[#E75F0B] dark:before:to-zinc-700 dark:before:from-zinc-800 border-2 border-white">
+          <Link to="/login" className="button-menu before:bg-gradient-to-tl before:to-[#C3480B] before:from-[#E75F0B] dark:before:to-zinc-700 dark:before:from-zinc-800 border-2 border-white">
             <span className="z-20">Iniciar</span>
-          </button>
+          </Link>
         </section>
       </header>
     </>
