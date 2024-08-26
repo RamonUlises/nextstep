d9cb96af031c064bf85fccba36bdc3305c253916
RamonUlises <jorgitomilina@gmail.com>
2024-08-25 19:40:05 -0600

    feat: Agregar funcionalidad de mostrar/ocultar contraseña en componente InputLogin

--- frontend/src/Components/Login/InputLogin.tsx
+++ frontend/src/Components/Login/InputLogin.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react';
+import { Eye } from '../../icons/Eye';
 
 export const InputLogin = ({
   text,
@@ -12,6 +13,7 @@ export const InputLogin = ({
   onInputChange: (event: { prop: string, value: string }) => void;
 }) => {
   const [value, setValue] = useState<number>(0);
+  const [eye, setEye] = useState<boolean>(false);
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const value = event.target.value;
@@ -21,18 +23,27 @@ export const InputLogin = ({
     onInputChange({ prop, value });
   }
 
+  function handleElementClick({ value }: { value: boolean }) {
+    setEye(value);
+  }
+
   return (
     <>
       <input
         required
         className="w-[250px] border-2 border-[#E75F0B] text-[#E75F0B] h-[50px] rounded-md text-sm py-0 px-4 bg-transparent outline-none"
         name={name}
-        type={type}
+        type={eye ? 'text' : type}
         onChange={handleChange}
       />
       <label className={`text-[#E75F0B] absolute top-[50%] left-[15px] translate-y-[-50%] text-sm pointer-events-none transition-all duration-300 ${value > 0 ? 'active' : ''}`}>
         {text}
       </label>
+      {
+        (type === 'password' && value > 0) ? (
+          <Eye styles='absolute top-[12px] right-[12px] cursor-pointer text-[#E75F0B]' onElementClick={handleElementClick} />
+        ) : null
+      }
     </>
   );
 };
