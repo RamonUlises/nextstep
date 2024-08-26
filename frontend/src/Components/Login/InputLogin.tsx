88609ff7bcc0add85ee9dd51b6c1c828e95845db
RamonUlises <jorgitomilina@gmail.com>
2024-08-25 19:11:45 -0600

    feat: Mejorar componente InputLogin en página de inicio de sesión

--- frontend/src/Components/Login/InputLogin.tsx
+++ frontend/src/Components/Login/InputLogin.tsx
@@ -1,8 +1,38 @@
-export const InputLogin = ({ text }: { text: string }) => {
+import { ChangeEvent, useState } from 'react';
+
+export const InputLogin = ({
+  text,
+  type,
+  name,
+  onInputChange,
+}: {
+  text: string;
+  type: string;
+  name: string;
+  onInputChange: (event: { prop: string, value: string }) => void;
+}) => {
+  const [value, setValue] = useState<number>(0);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const prop = event.target.name;
+
+    setValue(value.length);
+    onInputChange({ prop, value });
+  }
+
   return (
     <>
-      <input className="w-[250px] border-2 border-[#E75F0B] text-[#E75F0B] h-[50px] rounded-md text-sm py-0 px-4 bg-transparent outline-none" type="text" />
-      <label className="text-[#E75F0B] absolute top-[50%] left-[15px] translate-y-[-50%] text-sm pointer-events-none transition-all duration-300">{text}</label>
+      <input
+        required
+        className="w-[250px] border-2 border-[#E75F0B] text-[#E75F0B] h-[50px] rounded-md text-sm py-0 px-4 bg-transparent outline-none"
+        name={name}
+        type={type}
+        onChange={handleChange}
+      />
+      <label className={`text-[#E75F0B] absolute top-[50%] left-[15px] translate-y-[-50%] text-sm pointer-events-none transition-all duration-300 ${value > 0 ? 'active' : ''}`}>
+        {text}
+      </label>
     </>
   );
 };
--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -2,8 +2,15 @@ import { MenuDesktop } from '../Components/MenuDesktop';
 import { MenuMovil } from '../Components/MenuMovil';
 import { InputLogin } from '../Components/Login/InputLogin';
 import logo from '../assets/logo.png';
+import { useState } from 'react';
 
 export const Login = () => {
+  const [ inputs, setInputs ] = useState({ correo: '', contra: '' });
+
+  function onInputChange({ prop, value }: { prop: string, value: string }) {
+    setInputs({ ...inputs, [prop]: value });
+  }
+  
   return (
     <main className="flex flex-col h-screen overflow-hidden">
       <MenuDesktop />
@@ -16,10 +23,10 @@ export const Login = () => {
               Iniciar Sesión
             </h3>
             <div className="relative mt-5 input-google">
-              <InputLogin text="Correo eléctronico" />
+              <InputLogin text="Correo eléctronico" type='email' name='correo' onInputChange={onInputChange} />
             </div>
             <div className="relative mt-5 input-google">
-              <InputLogin text="Contraseña" />
+              <InputLogin text="Contraseña" type='password' name='contra' onInputChange={onInputChange} />
             </div>
             <button className="bg-[#E75F0B] p-3 rounded-xl text-white font-medium hover:bg-[#FA7C15] transition-colors duration-300   mt-12">
               Inicia sesión
