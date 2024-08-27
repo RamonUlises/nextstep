ecbb6bea03c15c3bb4e0640f2d488ad574d95f2f
RamonUlises <jorgitomilina@gmail.com>
2024-08-27 08:59:53 -0600

    feat: Mejorar estilos del menú de inicio de sesión

--- frontend/src/Components/Login/InputLogin.tsx
+++ frontend/src/Components/Login/InputLogin.tsx
@@ -31,17 +31,17 @@ export const InputLogin = ({
     <>
       <input
         required
-        className="w-[250px] border-2 border-[#E75F0B] text-[#E75F0B] h-[50px] rounded-md text-sm py-0 px-4 bg-transparent outline-none"
+        className="w-[250px] border-2 border-[#E75F0B] text-[#E75F0B] h-[50px] rounded-md text-sm py-0 px-4 bg-transparent outline-none dark:border-white dark:text-white"
         name={name}
         type={eye ? 'text' : type}
         onChange={handleChange}
       />
-      <label className={`text-[#E75F0B] absolute top-[50%] left-[15px] translate-y-[-50%] text-sm pointer-events-none transition-all duration-300 ${value > 0 ? 'active' : ''}`}>
+      <label className={`text-[#E75F0B] absolute top-[50%] left-[15px] translate-y-[-50%] text-sm pointer-events-none transition-all duration-300 dark:text-white ${value > 0 ? 'active' : ''}`}>
         {text}
       </label>
       {
         (type === 'password' && value > 0) ? (
-          <Eye styles='absolute top-[12px] right-[12px] cursor-pointer text-[#E75F0B]' onElementClick={handleElementClick} />
+          <Eye styles='absolute top-[12px] right-[12px] cursor-pointer text-[#E75F0B] dark:text-white' onElementClick={handleElementClick} />
         ) : null
       }
     </>
--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -17,10 +17,10 @@ export const Login = () => {
       <MenuMovil />
       <div className="flex h-full md:grid-rows-1 md:grid-cols-2 md:grid">
         <section className="hidden md:block bg-[url(./assets/image-login.png)] bg-cover bg-no-repeat"></section>
-        <section className="flex flex-col items-center w-full h-full md:justify-center">
+        <section className="flex flex-col items-center w-full h-full md:justify-center bg-gradient-to-r dark:from-zinc-800 dark:to-zinc-700">
           <img src={logo} alt="Logo NextStep" className="w-[60%] mt-4 md:mt-0 mb-4" />
-          <form className="flex flex-col items-center px-16 py-8 bg-white shadow-xl rounded-2xl ">
-            <h3 className="mt-6 mb-7 text-center text-[#E75F0B] text-2xl font-bold">
+          <form className="flex flex-col items-center px-16 py-8 bg-white shadow-xl rounded-2xl dark:bg-zinc-800">
+            <h3 className="mt-6 mb-7 text-center text-[#E75F0B] text-2xl font-bold dark:text-white">
               Iniciar Sesión
             </h3>
             <div className="relative mt-5 input-google">
@@ -39,7 +39,7 @@ export const Login = () => {
                 onInputChange={onInputChange}
               />
             </div>
-            <button className="bg-[#E75F0B] p-3 rounded-xl text-white font-medium hover:bg-[#FA7C15] transition-colors duration-300   mt-12">
+            <button className="bg-[#E75F0B] p-3 rounded-xl text-white font-medium hover:bg-[#FA7C15] transition-colors duration-300   mt-12 dark:bg-zinc-600 dark:hover:bg-zinc-500">
               Inicia sesión
             </button>
           </form>
