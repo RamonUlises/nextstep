5ef8a6aa58b49814238cb9bd0cb48958d2c99460
RamonUlises <jorgitomilina@gmail.com>
2024-08-28 08:12:24 -0600

    feat: Agregar tipos de datos para empresas, trabajadores y trabajos

---
+++ frontend/src/types/empresas.ts
@@ -0,0 +1,17 @@
+export interface TypeEmpresa {
+  'id': string;
+  'numero-identificacion': string;
+  'certificado-registro': string;
+  'licencia-comercial': string;
+  'nombre': string;
+  'direccion': string[];
+  'telefono': string[];
+  'email': string[];
+  'contrasena': string;
+  'redes-sociales': string[];
+  'mision': string;
+  'vision': string;
+  'objetivos': string[];
+  'puntuacion': number;
+  'imagen': string;
+};
---
+++ frontend/src/types/trabajos.ts
@@ -0,0 +1,14 @@
+export interface TypeTrabajos {
+  'id': string;
+  'empresa': string;
+  'descripcion': string;
+  'requerimientos': string[];
+  'fecha-publicacion': string;
+  'fecha-inicio': string;
+  'fecha-expiracion': string;
+  'contrato': string;
+  'presupuesto': number;
+  'puntos': number;
+  'etiquetas': string[];
+  'departamentos': string[];
+};
