579c9a09e30e64ca335ff4503063d92bf90f87fa
RamonUlises <jorgitomilina@gmail.com>
2024-08-28 08:41:13 -0600

    feat: Eliminar declaración global para obtenerEmpresas en window

--- frontend/src/lib/window.ts
+++
@@ -1,9 +0,0 @@
-import { TypeEmpresa } from '../types/empresas';
-
-declare global {
-  interface Window {
-    empresas: {
-      obtenerEmpresas: () => Promise<{ data: TypeEmpresa[]; status: number }>;
-    };
-  }
-}
