d7fac8e0ed1c16c17a196b135c955c8a625104c9
RamonUlises <jorgitomilina@gmail.com>
2024-08-30 12:13:21 -0600

    feat: Remove unused backend files and update authentication functionality

--- backend/dist/router/auth.js
+++
@@ -1,7 +0,0 @@
-import auth from '../controllers/auth.js';
-import { Router } from 'express';
-const router = Router();
-router.post('/login', (req, res) => {
-    void auth.login(req, res);
-});
-export default router;
