fe9e601b4e8c596dcb820e400aa69ca129b0b555
RamonUlises <jorgitomilina@gmail.com>
2024-09-02 17:01:35 -0600

    feat: Add lazy loading for images in TextInfo component

--- frontend/src/Components/Login/TextInfo.tsx
+++ frontend/src/Components/Login/TextInfo.tsx
@@ -1,25 +1,101 @@
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { textBackground } from '../../lib/textBackground';
 
 export const TextInfo = () => {
-  const text: string[] = 'Crea un perfil que destaque entre la multitud en StartUp'.split('');
+  const text: string[] =
+    'Crea un perfil que destaque entre la multitud en StartUp'.split('');
+  const text2: string[] =
+    'Tómate tu tiempo para crear tu perfil para que sea exactamente como quieres que sea.'.split(
+      ''
+    );
+  const text3: string[] =
+    'Aumente su credibilidad vinculando sus redes profesionales relevantes.'.split(
+      ''
+    );
+  const text4: string[] =
+    'Describe con precisión tus habilidades profesionales para ayudarte a conseguir más trabajo.'.split(
+      ''
+    );
+
+  const circleRef = useRef<HTMLDivElement>(null);
+  const spansRef = useRef<(HTMLSpanElement | null)[]>([]);
+  const spansLiRef1 = useRef<(HTMLSpanElement | null)[]>([]);
+  const spansLiRef2 = useRef<(HTMLSpanElement | null)[]>([]);
+  const spansLiRef3 = useRef<(HTMLSpanElement | null)[]>([]);
+
+  useEffect(() => {
+    textBackground({ circleRef, spansRef });
+    textBackground({ circleRef, spansRef: spansLiRef1 });
+    textBackground({ circleRef, spansRef: spansLiRef2 });
+    textBackground({ circleRef, spansRef: spansLiRef3 });
+
+    window.addEventListener('resize', () => {
+      textBackground({ circleRef, spansRef });
+      textBackground({ circleRef, spansRef: spansLiRef1 });
+      textBackground({ circleRef, spansRef: spansLiRef2 });
+      textBackground({ circleRef, spansRef: spansLiRef3 });
+    });
+    return () => {
+      window.removeEventListener('resize', () => {
+        textBackground({ circleRef, spansRef });
+        textBackground({ circleRef, spansRef: spansLiRef1 });
+        textBackground({ circleRef, spansRef: spansLiRef2 });
+        textBackground({ circleRef, spansRef: spansLiRef3 });
+      });
+    };
+  }, []);
 
   return (
     <>
-      <div className="w-[550px] h-[550px] rounded-full bg-gradient-to-r from-[#C3480B] to-[#E75F0B] absolute -top-[14%] -left-[5%] dark:to-zinc-700 dark:from-zinc-600 hidden md:block -z-10"></div>
+      <div
+        ref={circleRef}
+        className="w-[550px] h-[550px] rounded-full bg-gradient-to-r from-[#C3480B] to-[#E75F0B] absolute -top-[14%] -left-[5%] dark:to-zinc-700 dark:from-zinc-600 hidden md:block -z-10"
+      ></div>
       <h1 className="text-black text-3xl lg:text-5xl font-bold mb-[60px] dark:text-white">
-        {text.map(((letter, index) => <span data-span="span" key={index}>{letter}</span>))}
+        {text.map((letter, index) => (
+          <span
+            data-span="span"
+            key={index}
+            ref={(el) => (spansRef.current[index] = el)}
+          >
+            {letter}
+          </span>
+        ))}
       </h1>
       <ul className="flex flex-col text-black lg:text-xl ul-registro list-disc gap-6 font-semibold p-0 lg:px-[70px] dark:text-white">
-        <li className="">
-          Tómate tu tiempo para crear tu perfil para que sea exactamente como
-          quieres que sea.
+        <li>
+          {text2.map((letter, index) => (
+            <span
+              data-span="span"
+              key={index}
+              ref={(el) => (spansLiRef1.current[index] = el)}
+            >
+              {letter}
+            </span>
+          ))}
         </li>
         <li>
-          Aumente su credibilidad vinculando sus redes profesionales relevantes.
+          {text3.map((letter, index) => (
+            <span
+              data-span="span"
+              key={index}
+              ref={(el) => (spansLiRef2.current[index] = el)}
+            >
+              {letter}
+            </span>
+          ))}
         </li>
         <li>
-          Describe con precisión tus habilidades profesionales para ayudarte a
-          conseguir más trabajo.
+          {text4.map((letter, index) => (
+            <span
+              data-span="span"
+              key={index}
+              ref={(el) => (spansLiRef3.current[index] = el)}
+            >
+              {letter}
+            </span>
+          ))}
         </li>
       </ul>
 
