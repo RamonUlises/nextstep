be1a026232a395f59a9d37bd9363656c038e6ea6
RamonUlises <jorgitomilina@gmail.com>
2024-09-04 20:19:17 -0600

    feat: Mejorar funcionalidad de fondo de texto para una mejor legibilidad

--- frontend/src/Components/Login/TextInfo.tsx
+++ frontend/src/Components/Login/TextInfo.tsx
@@ -1,88 +1,17 @@
-import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { textBackground } from '../../utils/textBackground';
 import { text, text2, text3, text4 } from '../../lib/textsInfoLogin';
 
 export const TextInfo = () => {
-  const circleRef = useRef<HTMLDivElement>(null);
-  const spansRef = useRef<(HTMLSpanElement | null)[]>([]);
-  const spansLiRef1 = useRef<(HTMLSpanElement | null)[]>([]);
-  const spansLiRef2 = useRef<(HTMLSpanElement | null)[]>([]);
-  const spansLiRef3 = useRef<(HTMLSpanElement | null)[]>([]);
-
-  useEffect(() => {
-    textBackground({ circleRef, spansRef });
-    textBackground({ circleRef, spansRef: spansLiRef1 });
-    textBackground({ circleRef, spansRef: spansLiRef2 });
-    textBackground({ circleRef, spansRef: spansLiRef3 });
-
-    window.addEventListener('resize', () => {
-      textBackground({ circleRef, spansRef });
-      textBackground({ circleRef, spansRef: spansLiRef1 });
-      textBackground({ circleRef, spansRef: spansLiRef2 });
-      textBackground({ circleRef, spansRef: spansLiRef3 });
-    });
-    return () => {
-      window.removeEventListener('resize', () => {
-        textBackground({ circleRef, spansRef });
-        textBackground({ circleRef, spansRef: spansLiRef1 });
-        textBackground({ circleRef, spansRef: spansLiRef2 });
-        textBackground({ circleRef, spansRef: spansLiRef3 });
-      });
-    };
-  }, []);
-
   return (
     <>
-      <div
-        ref={circleRef}
-        className="w-[550px] h-[550px] rounded-full bg-gradient-to-r from-[#C3480B] to-[#E75F0B] absolute -top-[14%] -left-[5%] dark:to-zinc-700 dark:from-zinc-600 hidden md:block -z-10"
-      ></div>
+      <div className="w-[550px] h-[550px] rounded-full bg-gradient-to-r from-[#C3480B] to-[#E75F0B] absolute -top-[14%] -left-[5%] dark:to-zinc-700 dark:from-zinc-600 hidden md:block -z-10 opacity-95"></div>
       <h1 className="text-black text-3xl lg:text-5xl font-bold mb-[60px] dark:text-white">
-        {text.map((letter, index) => (
-          <span
-            data-span="span"
-            key={index}
-            ref={(el) => (spansRef.current[index] = el)}
-          >
-            {letter}
-          </span>
-        ))}
+        {text}
       </h1>
       <ul className="flex flex-col text-black lg:text-xl ul-registro list-disc gap-6 font-semibold p-0 lg:px-[70px] dark:text-white">
-        <li>
-          {text2.map((letter, index) => (
-            <span
-              data-span="span"
-              key={index}
-              ref={(el) => (spansLiRef1.current[index] = el)}
-            >
-              {letter}
-            </span>
-          ))}
-        </li>
-        <li>
-          {text3.map((letter, index) => (
-            <span
-              data-span="span"
-              key={index}
-              ref={(el) => (spansLiRef2.current[index] = el)}
-            >
-              {letter}
-            </span>
-          ))}
-        </li>
-        <li>
-          {text4.map((letter, index) => (
-            <span
-              data-span="span"
-              key={index}
-              ref={(el) => (spansLiRef3.current[index] = el)}
-            >
-              {letter}
-            </span>
-          ))}
-        </li>
+        <li>{text2}</li>
+        <li>{text3}</li>
+        <li>{text4}</li>
       </ul>
 
       <Link
