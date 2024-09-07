64f1414cbdbbee2d2d1409fd272206cc27ad5a2c
RamonUlises <jorgitomilina@gmail.com>
2024-09-07 09:52:21 -0600

    feat: Actualizar colores en tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,7 +12,19 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {}
+  		colors: {
+        'principal-50': '#FFF8ED',
+        'principal-100': '#FFEFD5',
+        'principal-200': '#FEDAAA',
+        'principal-300': '#FEBF73',
+        'principal-400': '#FC9A3B',
+        'principal-500': '#FA7C15',
+        'principal-600': '#E75F0B',
+        'principal-700': '#C3480B',
+        'principal-800': '#9B3911',
+        'principal-900': '#7C3112',
+        'principal-950': '#431607',
+      },
   	}
   },
   plugins: [require("tailwindcss-animate")],
