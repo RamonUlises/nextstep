1547c0910e324c995c6b9565537b8a06381d34c7
RamonUlises <jorgitomilina@gmail.com>
2024-09-07 21:09:52 -0600

    feat: Actualizar color de fondo del botón de agregar en el formulario de registro de colaborador

--- frontend/src/Pages/RegistroColaborador.tsx
+++ frontend/src/Pages/RegistroColaborador.tsx
@@ -11,27 +11,27 @@ export const RegistroColaborador = () => {
 
   return (
     <>
-      <main className="flex flex-col h-screen items-center px-3 overflow-y-auto overflow-x-hidden">
-        <h2 className="my-4 font-bold text-xl">Registrare</h2>
+      <main className="flex flex-col h-screen items-center px-3 overflow-y-auto overflow-x-hidden bg-gradient-to-tr dark:from-zinc-800 dark:to-zinc-700">
+        <h2 className="my-4 font-bold text-xl  dark:text-white">Regístrate</h2>
         <Tabs
           value={tab === 1 ? 'info-1' : 'info-2'}
           defaultValue="info-1"
-          className="w-full bg-slate-100 shadow-xl drop-shadow-md p-4 max-w-[900px]"
+          className="w-full bg-slate-100 shadow-xl drop-shadow-md p-4 max-w-[900px] dark:bg-zinc-800 rounded-xl"
         >
           <TabsList className="flex justify-center mb-4 items-center sm:gap-4 gap-2">
             <TabsTrigger
               className={`text-white w-12 h-12 sm:w-16 sm:h-16 rounded-full ${
-                tab === 1 ? 'bg-principal-600' : 'bg-slate-300'
+                tab === 1 ? 'bg-principal-600 dark:text-black dark:bg-white' : 'bg-slate-400 dark:text-white dark:bg-zinc-700'
               }`}
               value="info-1"
               onClick={() => setTab(1)}
             >
               1
             </TabsTrigger>
-            <div className="w-full max-w-[120px] h-1 bg-principal-600"></div>
+            <div className="w-full max-w-[120px] h-1 bg-principal-600 dark:bg-zinc-600"></div>
             <TabsTrigger
               className={`text-white w-12 h-12 sm:w-16 sm:h-16 rounded-full ${
-                tab === 2 ? 'bg-principal-600' : 'bg-slate-400'
+                tab === 2 ? 'bg-principal-600 dark:text-black dark:bg-white' : 'bg-slate-400 dark:text-white dark:bg-zinc-700'
               }`}
               value="info-2"
               onClick={() => setTab(2)}
