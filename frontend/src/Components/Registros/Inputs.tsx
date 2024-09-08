75fa105676836e99c1688f8a1a3538f10d6c24f1
RamonUlises <jorgitomilina@gmail.com>
2024-09-07 21:10:02 -0600

    Actualizar estilos de los campos de entrada en formularios

--- frontend/src/Components/Registros/Inputs.tsx
+++ frontend/src/Components/Registros/Inputs.tsx
@@ -12,11 +12,11 @@ export const Inputs = ({
       <section className="flex flex-col items-center w-full max-w-[400px] sm:px-4">
         <div className="w-full max-w-[300px]">
           <input
-            className="outline-none valid:font-light w-full border-b-2 bg-transparent border-principal-500"
+            className="outline-none valid:font-light w-full border-b-2 bg-transparent border-principal-500 dark:border-zinc-300"
             type={type}
             name={name}
           />
-          <label className="text-sm">{placeholder}</label>
+          <label className="text-sm  dark:text-white">{placeholder}</label>
         </div>
       </section>
     </>
--- frontend/src/Components/Registros/colaborador/Header1.tsx
+++ frontend/src/Components/Registros/colaborador/Header1.tsx
@@ -2,9 +2,9 @@ import { Image } from 'lucide-react';
 
 export const Header1 = () => {
   return (
-    <div className='sm:flex sm:px-4'>
-      <div className='sm:w-[70%] sm:flex sm:flex-col sm:justify-center'>
-        <h4 className="text-center font-semibold  sm:text-start sm:text-xl">Información personal</h4>
+    <div className='sm:flex sm:px-4 '>
+      <div className='sm:w-[70%] sm:flex sm:flex-col sm:justify-center dark:text-white'>
+        <h4 className="text-center font-semibold  sm:text-start sm:text-xl ">Información personal</h4>
         <p className="text-sm text-pretty">
           Cuéntanos un poco sobre ti. Esta información aparecerá en tu perfil
           público, para que los compradores potenciales puedan conocerte mejor.
@@ -12,12 +12,12 @@ export const Header1 = () => {
       </div>
       <div className="flex flex-col items-center sm:w-[30%] mt-2 sm:mt-0">
         <label className="cursor-pointer">
-          <Image className="bg-principal-600 text-white h-14 w-14 rounded-full p-3 mt-1" />
+          <Image className="bg-principal-600 text-white h-14 w-14 rounded-full p-3 mt-1 dark:bg-zinc-700" />
           <input type="file" hidden accept="image/*" />
         </label>
         <div>
-          <p className='text-sm'>foto de perfil</p>
-          <div className="h-1 w-full bg-principal-500"></div>
+          <p className='text-sm dark:text-white'>foto de perfil</p>
+          <div className="h-1 w-full bg-principal-500 dark:bg-zinc-200"></div>
         </div>
       </div>
       <div className='w-full sm:hidden h-[2px] bg-slate-200 mt-3'></div>
