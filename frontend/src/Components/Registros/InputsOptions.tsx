f26f0521cdfcbbf23ab5264e940a0274793ed908
RamonUlises <jorgitomilina@gmail.com>
2024-09-09 15:31:08 -0600

    feat: Update InputsOptions component with option prop

--- frontend/src/Components/Registros/InputsOptions.tsx
+++ frontend/src/Components/Registros/InputsOptions.tsx
@@ -3,11 +3,13 @@ import { Minus } from 'lucide-react';
 export const InputsOptions = ({
   index,
   value,
+  option,
   change,
   deleteInput,
 }: {
   index: number;
   value: string | undefined;
+  option: { id: string; value: string | undefined }[];
   change: ({ index, value }: { index: number; value: string }) => void;
   deleteInput: ({ index }: { index: number}) => void;
 }) => {
@@ -28,7 +30,9 @@ export const InputsOptions = ({
         value={value ?? ''}
         className="bg-transparent border-[3px] border-principal-300 rounded-sm outline-none focus:border-principal-200 px-2 w-full valid:font-light max-w-[250px]"
       />
-      <Minus onClick={deleteInp} width={22} height={22} className='bg-red-500 flex-grow-0 text-white rounded-full cursor-pointer' />
+      <button disabled={index === 0 && option.length === 1 } className={`w-[22px] h-[22px] ${(index === 0 && option.length === 1)? 'bg-red-300' : 'bg-red-500'} rounded-full`}>
+        <Minus onClick={deleteInp} width={22} height={22} className='flex-grow-0 text-white cursor-pointer' />
+      </button>
     </div>
   );
 };
