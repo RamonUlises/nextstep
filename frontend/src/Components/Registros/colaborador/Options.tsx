d997221283b4dc34e3fae79379bee5b85e8354de
RamonUlises <jorgitomilina@gmail.com>
2024-09-09 19:03:58 -0600

    feat: Update Options component to handle default option and setOption

--- frontend/src/Components/Registros/colaborador/Options.tsx
+++ frontend/src/Components/Registros/colaborador/Options.tsx
@@ -7,20 +7,36 @@ import {
   SelectValue,
 } from '../../ui/select';
 
-export const Options = ({ text } : { text: string}) => {
+export const Options = ({
+  text,
+  default: option,
+  setOption,
+}: {
+  text: string;
+  default: boolean;
+  setOption: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const handleOption = (value: string) => {
+    if (value === 'Si') {
+      setOption(true);
+    } else {
+      setOption(false);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center gap-4">
       <label>{text}</label>
-      <Select>
+      <Select onValueChange={handleOption}>
         <SelectTrigger className="w-[80px] justify-evenly border-none bg-slate-300 dark:bg-zinc-700 dark:text-white">
-          <SelectValue placeholder="No" />
+          <SelectValue placeholder={option ? 'Si' : 'No'} />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
-            <SelectItem className="cursor-pointer" value="si">
+            <SelectItem className="cursor-pointer" value="Si">
               Si
             </SelectItem>
-            <SelectItem className="cursor-pointer" value="no">
+            <SelectItem className="cursor-pointer" value="No">
               No
             </SelectItem>
           </SelectGroup>
