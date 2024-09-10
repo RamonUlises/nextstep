da722b0db59ffc0d548f6ef5400daa7cb255f9cb
RamonUlises <jorgitomilina@gmail.com>
2024-09-09 19:04:05 -0600

    feat: Update Inputs component to handle value and setProp props

--- frontend/src/Components/Registros/Inputs.tsx
+++ frontend/src/Components/Registros/Inputs.tsx
@@ -2,19 +2,25 @@ export const Inputs = ({
   type,
   placeholder,
   name,
+  value,
+  setProp,
 }: {
   type: string;
   placeholder: string;
   name: string;
+  value: string;
+  setProp: (React.Dispatch<React.SetStateAction<string>>) | undefined;
 }) => {
   return (
     <>
       <section className="flex flex-col items-center w-full max-w-[400px] sm:px-4">
         <div className="w-full max-w-[300px]">
           <input
+            onChange={(e) => setProp && setProp(e.target.value)}
             className="outline-none valid:font-light w-full border-b-2 bg-transparent border-principal-500 dark:border-zinc-300 dark:valid:text-white"
             type={type}
             name={name}
+            value={value}
           />
           <label className="text-sm  dark:text-white">{placeholder}</label>
         </div>
