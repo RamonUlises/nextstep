5f7541137f5fc62c56b4231285ed8bdd399f9280
RamonUlises <jorgitomilina@gmail.com>
2024-09-09 19:03:37 -0600

    feat: Add TypeColaboradores interface for defining collaborator data structure

---
+++ frontend/src/types/colaboradores.ts
@@ -0,0 +1,21 @@
+export interface TypeColaboradores {
+  'id': string;
+  'nombres': string;
+  'usuario': string;
+  'telefono': string;
+  'email': string;
+  'contrasena': string;
+  'redes-sociales': string[];
+  'imagen': string;
+  'descripcion': string;
+  'educacion-primaria': boolean;
+  'educacion-secundaria': boolean;
+  'titulos': string[];
+  'idiomas': string[];
+  'certificados': string[];
+  'referencias': string[];
+  'habilidades': string[];
+  'puntos': number;
+  'puntuacion': number;
+  'saldo': number;
+};
