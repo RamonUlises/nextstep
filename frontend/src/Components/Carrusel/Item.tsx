8bc6d08b5afbd58062763e14cbc69fb7b8378e3e
RamonUlises <jorgitomilina@gmail.com>
2024-09-15 13:26:30 -0600

    Actualizar estilos de la página de inicio y el carrusel

--- frontend/src/Components/Carrusel/Item.tsx
+++ frontend/src/Components/Carrusel/Item.tsx
@@ -13,8 +13,8 @@ export const Item = ({
     <div className="item">
       <img src={imagen} alt="" />
       <div className="content">
-        <h2 className="title">{title}</h2>
-        <p className="type">{type}</p>
+        <h2 className="title text-[4em] md:text-[5em] font-bold leading-[1.3]">{title}</h2>
+        <p className="type text-[2em] md:text-[3em] font-bold leading-[1.3]">{type}</p>
         <p className="description">{description}</p>
         <div className="button">
           <button>Ver más</button>
--- frontend/src/Components/MenuMovil.tsx
+++ frontend/src/Components/MenuMovil.tsx
@@ -8,7 +8,7 @@ import { OptionsMenuMovil } from './OptionsMenuMovil';
 export const MenuMovil = () => {
   return (
     <>
-      <header className="fixed bottom-0 left-0 flex justify-center w-full md:hidden z-50">
+      <header className="fixed bottom-0 left-0 flex justify-center w-full md:hidden z-[100]">
         <nav className="flex py-4 w-full justify-evenly bg-gradient-to-r from-[#C3480B] to-[#E75F0B] dark:from-zinc-800 dark:to-zinc-700 max-w-[450px] rounded-3xl mb-2">
           <Link to={'/'} className="rounded-full bg-[#FA7C15] p-1  hover:scale-125 transition-transform duration-300 dark:bg-zinc-600 w-[47px] h-[47px] flex justify-center">
             <Home />
