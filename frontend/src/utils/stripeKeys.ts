29a6cfd3e5520294406627e1b138ad8ede324e00
RamonUlises <jorgitomilina@gmail.com>
2024-09-28 18:01:51 -0600

    Agregar archivo stripeKeys.ts con claves de Stripe

---
+++ frontend/src/utils/stripeKeys.ts
@@ -0,0 +1,4 @@
+export default {
+  public: 'pk_test_51O87RjKZtU0tQP1o1txhVphJrnihU91DxQjWf5OubQB084SqBtQAR1S1nCw544g9X5oYEoxPqaAtnudAPkkZbBPb00MyHsCeXR',
+  private: 'sk_test_51O87RjKZtU0tQP1oYa04ldL1fiHGVDZFwqBasGXeAQlOTh2wGxv7aITDJ4a2vW7FYHHOnfqqtBKOSOw17hgEvJpE009Of6XliZ'
+};
